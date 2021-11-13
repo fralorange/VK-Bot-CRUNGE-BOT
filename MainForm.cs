4724aab2349c67720fa9434919471119473bb2a9
fralorange <fralorange@mail.ru>
2021-11-14 00:05:03 +0300

    Revert "Fresh new start"
    This reverts commit 40d7faf1b789363938620714409322a194045086.

--- MainForm.cs
+++ MainForm.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace VK_Control_Panel_Bot
+namespace VK_Bot_CRUNGE_BOT
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace VK_Control_Panel_Bot
+namespace VK_Bot_CRUNGE_BOT
 {
     internal static class Program
     {
@@ -17,7 +17,7 @@ namespace VK_Control_Panel_Bot
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
