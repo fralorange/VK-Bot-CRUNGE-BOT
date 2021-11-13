3d9ef8ed9865953f5668d38242b8a118087ea937
fralorange <fralorange@mail.ru>
2021-11-13 22:32:52 +0300

    Prj created fin

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace VK_Bot_CRUNGE_BOT
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
